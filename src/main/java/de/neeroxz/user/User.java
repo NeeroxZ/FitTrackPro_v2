e05b7d14900c03dd5fbe13116a2d6835ad07c4d9
NeeroxZ <neeroxz@hotmail.de>
2024-10-19 19:55:19 +0200

    record: User

---
+++ src/main/java/de/neeroxz/user/User.java
@@ -0,0 +1,10 @@
+package de.neeroxz.user;
+
+/**
+ * Class: User
+ *
+ * @author NeeroxZ
+ * @date 19.10.2024
+ */
+public record User(String username, Password password, double gewicht, double grosse, Birthday geburtstag) {}
+
