064c2ef56364fb95181ccfaf66d814c045d476a1
NeeroxZ <neeroxz@hotmail.de>
2024-10-19 20:12:41 +0200

    Start init Database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import de.neeroxz.db.H2InitDatabase;
 import de.neeroxz.ui.AppPanel;
 
 /**
@@ -9,6 +10,8 @@ import de.neeroxz.ui.AppPanel;
 public class App {
     public static void main(String[] args) {
         System.out.println("test");
+        H2InitDatabase db = new H2InitDatabase();
+        db.initDatabase();
         new AppPanel().showPanel();
     }
 }
