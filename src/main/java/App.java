9e95d165fa4549eba6a108f1416761341bc14fb1
NeeroxZ <neeroxz@hotmail.de>
2024-10-19 19:27:51 +0200

    Move App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import de.neeroxz.ui.AppPanel;
+
+/**
+ * Class: App
+ *
+ * @author NeeroxZ
+ * @date 19.10.2024
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("test");
+        new AppPanel().showPanel();
+    }
+}
