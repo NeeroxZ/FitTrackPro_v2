0e37da7151a7b3f93795b1406a9ebe42ade3dff4
NeeroxZ <neeroxz@hotmail.de>
2024-10-21 10:31:43 +0200

    Record für eine Exercise

---
+++ src/main/java/de/neeroxz/exercise/Exercise.java
@@ -0,0 +1,12 @@
+package de.neeroxz.exercise;
+
+/**
+ * Record: Exercise
+ *
+ * @author NeeroxZ
+ * @date 21.10.2024
+ */
+
+public record Exercise(int id, String name, ExerciseCategory category, String difficulty) {
+}
+
