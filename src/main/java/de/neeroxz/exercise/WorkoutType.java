d9b074da3cb41a2e6040c5d1058b822eb3e35d51
NeeroxZ <neeroxz@hotmail.de>
2024-10-21 10:30:39 +0200

    Enum für übergeordnete WorkoutType

---
+++ src/main/java/de/neeroxz/exercise/WorkoutType.java
@@ -0,0 +1,15 @@
+package de.neeroxz.exercise;
+
+/**
+ * Enum: WorkoutType
+ *
+ * @author NeeroxZ
+ * @date 21.10.2024
+ */
+
+public enum WorkoutType {
+    KRAFTSPORT,
+    CARDIO,
+    YOGA
+}
+
