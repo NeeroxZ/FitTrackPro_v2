f8962ae5aae72c89422c8d39c6e39bc8465412a5
NeeroxZ <neeroxz@hotmail.de>
2024-10-21 10:33:14 +0200

    class: Service für Übungen nach SOLID prinzip

---
+++ src/main/java/de/neeroxz/exercise/ExerciseService.java
@@ -0,0 +1,26 @@
+package de.neeroxz.exercise;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Class: ExerciseService
+ *
+ * @author NeeroxZ
+ * @date 21.10.2024
+ */
+public class ExerciseService {
+    private final ExerciseRepository exerciseRepository;
+
+    public ExerciseService(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    public Optional<Exercise> getExerciseById(int id) {
+        return exerciseRepository.findById(id);
+    }
+
+    public List<Exercise> getAllExercises() {
+        return exerciseRepository.findAll();
+    }
+}
