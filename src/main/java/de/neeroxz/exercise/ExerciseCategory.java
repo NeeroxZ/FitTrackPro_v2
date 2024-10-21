5bcf76ee2187fcd23c1a13b6ebbfa4590666b7a6
NeeroxZ <neeroxz@hotmail.de>
2024-10-21 10:31:06 +0200

    Enum für subtypen des WorkoutType -> ExerciseCategory

---
+++ src/main/java/de/neeroxz/exercise/ExerciseCategory.java
@@ -0,0 +1,32 @@
+package de.neeroxz.exercise;
+
+/**
+ * Enum: ExerciseCategory
+ *
+ * @author NeeroxZ
+ * @date 21.10.2024
+ */
+
+public enum ExerciseCategory {
+    // Kraftsport Unterkategorien
+    BRUST(WorkoutType.KRAFTSPORT),
+    RUECKEN(WorkoutType.KRAFTSPORT),
+    BEINE(WorkoutType.KRAFTSPORT),
+
+    // Cardio-Kategorien
+    CARDIO(WorkoutType.CARDIO),
+
+    // Yoga-Kategorien
+    YOGA(WorkoutType.YOGA);
+
+    private final WorkoutType workoutType;
+
+    ExerciseCategory(WorkoutType workoutType) {
+        this.workoutType = workoutType;
+    }
+
+    public WorkoutType getWorkoutType() {
+        return workoutType;
+    }
+}
+
