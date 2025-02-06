d4c076a9db314e76a94a44f2a73819aa232eb09b
NeeroxZ <neeroxz@hotmail.de>
2025-02-06 16:31:20 +0100

    class: hinzufuegen UserService

---
+++ src/main/java/de/neeroxz/user/UserService.java
@@ -0,0 +1,32 @@
+package de.neeroxz.user;
+
+import java.util.Optional;
+
+/**
+ * Class: UserService
+ *
+ * @author NeeroxZ
+ * @date 06.02.2025
+ */
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByUsernameAndPassword(String username, Password password) {
+       return userRepository.findUserByUsernameAndPassword(username, password);
+    }
+
+    @Override
+    public void saveUser(User user) {
+
+    }
+}
