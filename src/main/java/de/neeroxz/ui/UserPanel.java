cb4fad6c22a36f3c1f181fb1d8bbca7acce385d6
NeeroxZ <neeroxz@hotmail.de>
2025-03-04 09:08:58 +0100

    feat(deleteUserPanel): Panel zum Anzeigen/Nachfragen ob der User gelöscht werden soll

--- src/main/java/de/neeroxz/ui/UserPanel.java
+++ src/main/java/de/neeroxz/ui/UserPanel.java
@@ -1,5 +1,11 @@
 package de.neeroxz.ui;
 
+import de.neeroxz.exercise.WorkoutType;
+import de.neeroxz.user.IUserService;
+import de.neeroxz.user.LoggedInUser;
+
+import java.util.Scanner;
+
 /**
  * Class: UserPanel
  *
@@ -8,8 +14,10 @@ package de.neeroxz.ui;
  */
 public class UserPanel extends AbstractConsolePanel{
 
-    public UserPanel()
+    IUserService userService;
+    public UserPanel(IUserService userService)
     {
+        this.userService = userService;
         super.addMenuAction(
                 "Gewicht ändern",
                 this::changeWeight
@@ -28,6 +36,24 @@ public class UserPanel extends AbstractConsolePanel{
     {
 
         System.out.println("Möchten Sie ihren Account Wirklich löschen");
+        System.out.println("1. Ja");
+        System.out.println("2. Nein");
+        System.out.print("Deine Wahl: ");
+        Scanner scanner = new Scanner(System.in);//todo Eventuell löschen ?
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Zeilenumbruch entfernen
+
+        switch (choice)
+        {
+            case 1: userService.deleteUser( //todo gehört das hier rein ?
+                    LoggedInUser
+                            .getCurrentUser()
+                            .orElseThrow(
+                                    () -> new IllegalStateException("Kein User eingeloggt!")
+                            )
+                    );
+            case 2: super.handleInput();
+        }
     }
 
     private void changeUserName()
