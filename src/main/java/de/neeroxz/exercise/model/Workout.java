67aaccfb183b44b49216916f16b274dca172db12
NeeroxZ <neeroxz@hotmail.de>
2025-03-12 11:17:06 +0100

    refactor(model): Rename exercises to trainingDays in Workout for better clarity"

--- src/main/java/de/neeroxz/exercise/model/Workout.java
+++ src/main/java/de/neeroxz/exercise/model/Workout.java
@@ -7,13 +7,15 @@ package de.neeroxz.exercise.model;
  * @date 03.02.2025
  */
 
+import de.neeroxz.exercise.service.generator.SmarterWorkoutGenerator;
+
 import java.util.List;
 
 public record Workout(
         int id,
         String name,
         WorkoutType type,
-        List<Exercise> exercises,
+        List<TrainingDay> trainingDays,
         String username,
         int frequency,          // Anzahl Trainingstage pro Woche (1-7)
         TrainingSplit split)
