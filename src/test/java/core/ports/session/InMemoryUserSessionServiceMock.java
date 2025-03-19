582e8221594647223d2da194c1960d5f3ffd8431
NeeroxZ <neeroxz@hotmail.de>
2025-03-19 10:54:30 +0100

    test(InMemoryUserSessionServiceMock): Erstellt und noch nicht getestet das In-Memory Mock für Übungen

---
+++ src/test/java/core/ports/session/InMemoryUserSessionServiceMock.java
@@ -0,0 +1,39 @@
+package core.ports.session;
+
+import core.domain.user.User;
+
+import java.util.Optional;
+
+/**
+ * Simuliert einen eingeloggten Benutzer für Tests.
+ */
+public class InMemoryUserSessionServiceMock implements IUserSessionService {
+
+    private User loggedInUser;
+
+    @Override
+    public Optional<User> getLoggedInUser() {
+        return Optional.ofNullable(loggedInUser);
+    }
+
+    @Override
+    public boolean isLoggedIn() {
+        return loggedInUser != null;
+    }
+
+    @Override
+    public void logout() {
+        loggedInUser = null;
+    }
+
+    @Override
+    public void setLoggedInUser(User updatedUser) {
+        this.loggedInUser = updatedUser;
+    }
+
+    @Override
+    public String getCurrentUsername()
+    {
+        return loggedInUser.username();
+    }
+}
