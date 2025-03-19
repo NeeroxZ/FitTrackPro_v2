a8a6a1de8c54bdd940628fce4603fd3d2b21d41d
NeeroxZ <neeroxz@hotmail.de>
2025-03-19 12:36:13 +0100

    Add(GetExercisesByTypeUseCase)

--- src/main/java/core/usecase/exercise/GetExercisesByTypeUseCase.java
+++ src/main/java/core/usecase/exercise/GetExercisesByTypeUseCase.java
@@ -1,4 +1,24 @@
 package core.usecase.exercise;
 
+import core.domain.exercise.Exercise;
+import core.domain.workout.WorkoutType;
+import core.ports.repository.IExerciseRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GetExercisesByTypeUseCase {
+    private final IExerciseRepository exerciseRepository;
+
+    public GetExercisesByTypeUseCase(IExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    public List<Exercise> execute(WorkoutType type) {
+        return exerciseRepository.findAll()
+                                 .stream()
+                                 .filter(e -> e.category().getWorkoutType() == type)
+                                 .collect(Collectors.toList());
+    }
 }
+
