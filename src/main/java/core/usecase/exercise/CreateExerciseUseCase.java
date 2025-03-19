5d4f9fbfcf21bfc4f27810a36a5d9ed98d728e14
NeeroxZ <neeroxz@hotmail.de>
2025-03-19 12:37:42 +0100

    Add(CreateExerciseUseCase)

--- src/main/java/core/usecase/exercise/CreateExerciseUseCase.java
+++ src/main/java/core/usecase/exercise/CreateExerciseUseCase.java
@@ -1,5 +1,16 @@
 package core.usecase.exercise;
 
-public class CreateExerciseUseCase
-{
+import core.domain.exercise.Exercise;
+import core.ports.repository.IExerciseRepository;
+
+public class CreateExerciseUseCase {
+    private final IExerciseRepository exerciseRepository;
+
+    public CreateExerciseUseCase(IExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    public void execute(Exercise exercise) {
+        exerciseRepository.addExercise(exercise);
+    }
 }
