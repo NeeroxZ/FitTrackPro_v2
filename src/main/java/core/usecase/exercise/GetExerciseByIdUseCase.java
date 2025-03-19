827ee20dddfd08f91f357461d8f4ed83f3014430
NeeroxZ <neeroxz@hotmail.de>
2025-03-19 12:36:26 +0100

    Add(GetExerciseByIdUseCase)

--- src/main/java/core/usecase/exercise/GetExerciseByIdUseCase.java
+++ src/main/java/core/usecase/exercise/GetExerciseByIdUseCase.java
@@ -1,4 +1,18 @@
 package core.usecase.exercise;
 
+import core.domain.exercise.Exercise;
+import core.ports.repository.IExerciseRepository;
+
+import java.util.Optional;
+
 public class GetExerciseByIdUseCase {
+    private final IExerciseRepository exerciseRepository;
+
+    public GetExerciseByIdUseCase(IExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    public Optional<Exercise> execute(int id) {
+        return exerciseRepository.findById(id);
+    }
 }
